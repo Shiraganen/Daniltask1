188298cef91eaccb34524ab4d9a8ce106c99cdca
ViktoriaReyna <dbrnjhbz555242@gmail.com>
2025-01-23 18:43:31 +0300

    button submit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('#form');
+const taskinput = document.querySelector('#taskinput');
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+})
